3c06a7aba5662c1ebeafb9b4bbaa38447c5817ec
Avala Parameswara Reddy <avalaparameswarareddy@gmail.com>
2026-08-14 07:18:36 +0530

    fix(m3b1): resolve capability entropy from trusted extensions schema

---
+++ supabase/migrations/20260814014815_m3b1_pgcrypto_capability_search_path.sql
@@ -0,0 +1,4 @@
+alter function creator_private.creator_claim_mock_voice_job_b1_impl(uuid,integer)
+  set search_path = 'extensions';
+alter function creator_private.creator_claim_media_deletion_impl(uuid,integer)
+  set search_path = 'extensions';
